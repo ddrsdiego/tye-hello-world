10eef67b9c5d0cbd435360fba7db860c751db491
ddrsDiego <ddrsdiego@hotmail.com>
2022-12-25 20:31:41 -0300

    remove usings não utilizados

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,17 +1,32 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
-
 namespace WebApp
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    using System.Reflection;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.OpenApi.Models;
+    using Repositories;
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+    public static class Program
+    {
+        public static async Task Main(string[] args) => await CreateHostBuilder(args).Build().RunAsync();
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); })
+                .ConfigureServices((ctx, services) =>
+                {
+                    services.AddDaprClient(builder =>
+                    {
+                        builder.UseHttpEndpoint("http://localhost:5010");
+                        builder.UseGrpcEndpoint("http://localhost:50010");
+                    });
+                    services.AddControllers();
+                    services.AddSingleton<IWeatherForecastRepository, WeatherForecastRepository>();
+            
+                    services.AddSwaggerGen(c =>
+                        c.SwaggerDoc("v1", new OpenApiInfo {Title = Assembly.GetEntryAssembly()?.GetName().Name}));
+                });
     }
 }
--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -3,41 +3,16 @@ namespace WebApp
     using System.Reflection;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
-    using Microsoft.Extensions.Configuration;
-    using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
-    using Microsoft.OpenApi.Models;
-    using Repositories;
 
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDaprClient(builder =>
-            {
-                builder.UseHttpEndpoint("http://localhost:5010");
-                builder.UseGrpcEndpoint("http://localhost:50010");
-            });
-            services.AddControllers();
-            services.AddSingleton<IWeatherForecastRepository, WeatherForecastRepository>();
-            
-            services.AddSwaggerGen(c =>
-                c.SwaggerDoc("v1", new OpenApiInfo {Title = Assembly.GetEntryAssembly()?.GetName().Name}));
-        }
-
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                
+
                 app.UseSwagger();
                 app.UseSwaggerUI(c =>
                     c.SwaggerEndpoint("/swagger/v1/swagger.json", Assembly.GetEntryAssembly()?.GetName().Name));
@@ -46,9 +21,7 @@ namespace WebApp
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
